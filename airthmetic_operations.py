dbece82a662fce6d6b1fee6207ba2e6714b4e482
vaibhawdixit <vaibhaw.dixit2024@gmail.com>
2026-02-01 20:46:18 +0530

    Python Basics Assignment

---
+++ airthmetic_operations.py
@@ -0,0 +1,14 @@
+a=input("Enter the first number :")
+b=input("Enter the second number :")
+
+add =int(a)+int(b)
+
+print("Addition of Two number is : ",add)
+
+sub=int(a)-int(b)
+print("Subtraction of Two number is : ",sub)
+
+mult=int(a)*int(b)
+print("Multipication of Two number is : ",mult)
+div=int(a)/int(b)
+print("Divison of Two number is : ",div)
---
+++ greetings.py
@@ -0,0 +1,4 @@
+first_name =input("Enter Your First Name : ")
+last_name = input("Enter Your Last Name : ")
+print("Hello,",first_name,last_name,"!","Welcome to Python program.")
+
