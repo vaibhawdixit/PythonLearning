6b861e0275d0f17d7687a35cd25dcfdaf03cf4a2
vaibhawdixit <vaibhaw.dixit2024@gmail.com>
2026-02-15 19:50:35 +0530

    Assignment 2

---
+++ prime_number_finder.py
@@ -0,0 +1,5 @@
+a=int(input("enter a  umber between 0 and 100 : "))
+if a%2==0:
+    print(f"{a} is an even number")
+else:
+    print(f"{a} is an odd number")
---
+++ sum_of_numbers.py
@@ -0,0 +1,4 @@
+sum1 = 0
+for i in range(1, 50):
+    sum1 = sum1 + i
+print(f"The sum of numbers from 1 to 50 is : {sum1}")
